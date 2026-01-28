b6ba33d10c65e4ee56c3dd160698cd4a49a0ec22
MilanFox <milan.fox@linuxmail.org>
2026-01-28 22:19:41 +0100

    add basic seo features

---
+++ app/layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <NuxtPage />
+</template>
+
+<script setup lang="ts">
+import { useLocaleHead } from '#i18n';
+
+const i18nHead = useLocaleHead({ lang: true, dir: true, seo: true });
+
+useHead(() => ({
+  htmlAttrs: i18nHead.value.htmlAttrs,
+  meta: i18nHead.value.meta,
+  link: i18nHead.value.link,
+}));
+</script>
