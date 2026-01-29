4dbe87fd171c8dc632275eaca9277af51859d5ff
Milan Fox <milan.fox@linuxmail.org>
2026-01-29 17:03:57 +0100

    add "inter" font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import { resolve } from 'path';
 import { routes } from './config/routes';
 
 export default defineNuxtConfig({
@@ -11,7 +10,11 @@ export default defineNuxtConfig({
 
   app: {
     head: {
-      link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }],
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/favicon.png' },
+        { rel: 'preload', href: '/fonts/inter/InterVariable.woff2', as: 'font', type: 'font/woff2', crossorigin: 'anonymous' },
+        { rel: 'preload', href: '/fonts/inter/InterVariable-Italic.woff2', as: 'font', type: 'font/woff2', crossorigin: 'anonymous' },
+      ],
       title: 'Terra Incognita',
       titleTemplate: '%s · Terra Incognita',
     },
@@ -19,23 +22,8 @@ export default defineNuxtConfig({
 
   css: ['~/assets/scss/main.scss'],
 
-  alias: {
-    '@assets': resolve(__dirname, './assets'),
-    '@config': resolve(__dirname, './config'),
-    '@components': resolve(__dirname, './app/components'),
-    '@atoms': resolve(__dirname, './app/components/atoms'),
-    '@molecules': resolve(__dirname, './app/components/molecules'),
-    '@organisms': resolve(__dirname, './app/components/organisms'),
-    '@stores': resolve(__dirname, './app/stores'),
-    '@composables': resolve(__dirname, './app/composables'),
-    '@constants': resolve(__dirname, './app/constants'),
-    '@server': resolve(__dirname, './server'),
-    '@layouts': resolve(__dirname, './app/layouts'),
-    '@middleware': resolve(__dirname, './app/middleware'),
-  },
-
   routeRules: {
-    [`${routes.STYLEGUIDE}/**`]: { appLayout: 'styleguide' },
+    [`/**${routes.STYLEGUIDE}/**`]: { appLayout: 'styleguide' },
   },
 
   compatibilityDate: '2024-04-03',
@@ -44,11 +32,12 @@ export default defineNuxtConfig({
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: '@use "~/assets/scss/variables.scss" as *;',
+          additionalData: '@use "~/assets/scss/variables" as *;',
         },
       },
     },
   },
+
   postcss: {
     plugins: {
       'postcss-pxtorem': {
@@ -69,6 +58,7 @@ export default defineNuxtConfig({
   },
 
   i18n: {
+    baseUrl: 'https://terra-incognita.vercel.app/',
     strategy: 'prefix',
     defaultLocale: 'de',
     locales: [
