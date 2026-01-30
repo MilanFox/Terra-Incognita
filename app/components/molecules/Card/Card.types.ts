25fdc8c86396ae84c91cf6c12d1b068f4e96fdd7
Milan Fox <milan.fox@linuxmail.org>
2026-01-30 16:23:07 +0100

    add basic Card component

---
+++ app/components/molecules/Card/Card.types.ts
@@ -0,0 +1,11 @@
+import type { UIButtonColor } from '@atoms/UIButton/UIButton.types';
+
+export interface CardProps {
+  title?: string
+  description?: string
+  color?: UIButtonColor
+  link?: {
+    text: string
+    to: string
+  }
+}
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,39 @@
 <template>
-  <Hero />
+  <div class="home">
+    <Hero class="home__hero" />
+
+    <div class="home__content">
+      <div class="home__cards">
+        <Card
+          color="blue"
+          title="Lorem ipsum dolor sit amet"
+          description="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+          :link="{ text: 'Placeholder Link', to: '/' }"
+        />
+
+        <Card color="orange" />
+      </div>
+    </div>
+  </div>
 </template>
+
+<style lang="scss">
+.home {
+  &__content {
+    @include container();
+
+    > * {
+      margin-top: 32px;
+    }
+  }
+
+  &__cards {
+    display: grid;
+    gap: 32px;
+
+    @include breakpoint(tablet) {
+      grid-template-columns: 1fr 1fr;
+    }
+  }
+}
+</style>
