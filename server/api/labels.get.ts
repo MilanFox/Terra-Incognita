1fb4ea7e1ee26f61f266fceed7dc47969e046ff6
Milan Fox <milan.fox@linuxmail.org>
2026-02-03 14:30:11 +0100

    add endpoint for fetching country labels

--- server/api/labels.get.ts
+++ server/api/labels.get.ts
@@ -1,7 +1,7 @@
 import { supabase } from '#server/utils/supabase';
 
 export default defineEventHandler(async () => {
-  const { data, error } = await supabase.rpc('sovereign_countries');
+  const { data, error } = await supabase.rpc('get_labels');
 
   if (error) {
     throw createError({ statusCode: 500, statusMessage: error.message });
